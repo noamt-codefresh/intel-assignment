eb2755e4ed13ee6a88f1f0d04859c7baa7a8ef01
noamdoron <noahdoron@gmail.com>
2019-12-20 20:45:50 +0200

    -minor css fixes

--- todo-ui/src/app/_component/login/login.component.ts
+++ todo-ui/src/app/_component/login/login.component.ts
@@ -24,7 +24,7 @@ export class LoginComponent implements OnInit {
         this.authService.setUser(response);
     }, error => {
       console.error(error);
-      alert(error);
+      alert(error.message);
     });
   }
 
